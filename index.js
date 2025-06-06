1ce3ac12b69461cb5db5f7706710abc82e534173
Bdtranter <bdtranter@oakland.edu>
2025-06-06 19:52:46 -0400

    Initial Commit. Minimal Project for learning about RESTful APIs. Added my first get endpoint to the tshirt URI

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = require('express')();
+const PORT = 8080;
+
+app.listen(
+    PORT,
+    () => console.log(`its alive on http://localhost:${PORT}`)
+)
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: 'shirt',
+        size: 'large'
+    })
+});
